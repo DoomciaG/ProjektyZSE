c582b3036b4185146d77ca6d7ff84709358544f7
student <Kooteks@wp.pl>
2017-03-15 18:44:52 +0100

    tududu

--- app/src/main/java/com/mati/nowaapka/scena1.java
+++ app/src/main/java/com/mati/nowaapka/scena1.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by uczen on 2017-03-11.
@@ -13,6 +14,7 @@ import android.widget.Button;
 public class scena1 extends AppCompatActivity {
 
     Button P1;
+
 
 
     @Override
